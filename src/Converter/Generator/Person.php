73172d232185c230a1d6162abedeec3dad7e456e
Tobias Hille <t.hille@gmail.com>
2024-10-08 13:05:28 +0200

    Try another method to trigger firstName and lastName from Faker

--- src/Converter/Generator/Person.php
+++ src/Converter/Generator/Person.php
@@ -4,15 +4,23 @@ declare(strict_types=1);
 
 namespace Smile\GdprDump\Converter\Generator;
 
-use Faker\Generator;
 use Smile\GdprDump\Converter\ConverterInterface;
+use Smile\GdprDump\Converter\Proxy\Faker;
+use Smile\GdprDump\Faker\FakerService;
 
 class Person implements ConverterInterface
 {
     public function convert(mixed $value, array $context = []): string
     {
-        $person = new \Faker\Provider\Person(new Generator());
-        return $person->firstName() . $person->lastName();
+        $converter = new Faker(new FakerService());
+        $converter->setParameters(['firstName']);
+        $firstName = $converter->convert($value, $context);
+
+        $converter = new Faker(new FakerService());
+        $converter->setParameters(['firstName']);
+        $lastName = $converter->convert($value, $context);
+
+        return 'test tester'; //$firstName . ' ' . $lastName;
     }
 
     public function setParameters(array $parameters): void
